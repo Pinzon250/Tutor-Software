7fb62710b6d10c183e94efabc5689cdd36d97d52
Pinzon250 <santiagopinzo@gmail.com>
2025-05-01 20:20:25 -0500

    Bases para empezar

---
+++ frontend/electron/main.cjs
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      contextIsolation: true,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL("http://localhost:5173"); // Vite dev server
+  } else {
+    win.loadFile(path.join(__dirname, "../dist/index.html")); // build de Vite
+  }
+}
+
+app.whenReady().then(createWindow);
