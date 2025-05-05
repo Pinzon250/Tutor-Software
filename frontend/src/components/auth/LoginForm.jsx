96f3e85d516239fc2a86543db4e10761e3e6214f
Pinzon250 <santiagopinzo@gmail.com>
2025-05-05 12:53:36 -0500

    Inicio de sesion terminado (front)

--- frontend/src/components/auth/LoginForm.jsx
+++ frontend/src/components/auth/LoginForm.jsx
@@ -30,27 +30,47 @@ function LoginForm() {
   };
 
   return (
-
+    <div className='-mt-3 '><button className='font-semibold text-green-600 hover:text-green-900' onClick={() => navigate('/')}>Regresar</button>
     <form onSubmit={handleSubmit} className="space-y-4 p-4 max-w-sm mx-auto">
-        <button onClick={() => navigate('/')} className=''>Regresar</button>
-      <h2 className="text-xl font-bold text-center">Iniciar sesión</h2>
+      <h2 className="text-xl text-green-600 font-bold text-center">Iniciar sesión</h2>
       <input
         type="text"
         name="username"
         placeholder="Correo"
         onChange={handleChange}
-        className="w-full border p-2"
+        className="border rounded border-gray-300 w-full p-2 bg-white/50 focus:ring-2 focus:ring-green-600 outline-none"
       />
       <input
         type="password"
         name="password"
         placeholder="Contraseña"
         onChange={handleChange}
-        className="w-full border p-2"
+        className="border rounded border-gray-300 w-full p-2 bg-white/50 focus:ring-2 focus:ring-green-600 outline-none"
       />
-      <button className="bg-green-600 text-white w-full p-2">Entrar</button>
+      <div className='mb-4 flex items-center'>
+           <input
+               id="LoginRemember"
+               type="checkbox"
+               name="remember"
+               className="mr-2 -mt-2 bg-[#C19A6B]"
+           />
+             <label htmlFor="LoginRemember" className="text-sm -mt-2 text-gray-700">Remember Me</label>
+       </div>
+      <button className="w-full cursor-pointer bg-green-600 hover:bg-green-500 text-white font-semibold py-2 px-4 rounded-lg rounded transition duration-300 ">Entrar</button>
       {error && <p className="text-red-500">{error}</p>}
     </form>
+    <aside className="mt-4 text-sm text-gray-600">
+      <p className="flex justify-between -mt-5">
+            <a href="/forgot" className="text-green-600 ml-5 cursor-pointer hover:text-green-900 font-semibold ml-1">Forgot Password?</a>
+            <p>Don't have an account?
+            <a href="/Register" className="text-green-600 mr-5 cursor-pointer hover:text-green-900 font-semibold ml-1">Register</a>
+            </p>
+        </p>
+        <p className='text-center text-gray-500 pt-9'>
+            By signing in, you agree to our <a href="/terms" className="text-green-600 hover:text-green-900 font-semibold">Terms of Service</a> and <a href="/privacy" className="text-green-600 hover:text-green-900 font-semibold">Privacy Policy</a>.
+        </p>
+    </aside>
+    </div>
   );
 }
 
--- frontend/src/pages/user/Log_Reg.jsx
+++ frontend/src/pages/user/Log_Reg.jsx
@@ -10,7 +10,7 @@ const Log_Reg = () => {
 
         <VantaNetBackground />
         
-      <div className="px-6 py-10 bg-white/20 rounded-xl backdrop-blur-sm relative z-10 text-center drop-shadow-xl/50">
+      <div className="px-6 py-10 bg-white/40 rounded-xl backdrop-blur-sm relative z-10 text-center">
         <h1 className="text-4xl md:text-5xl text-shadow-sm font-extrabold text-green-500 text-shadow-green-600 mb-3">
           Bienvenido al Tutor Software de Redes
         </h1>
@@ -21,7 +21,7 @@ const Log_Reg = () => {
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
           <button
             onClick={() => navigate('/login')}
-            className="px-6 py-3 bg-white/30 backdrop-blur-sm text-gray-700 rounded-xl shadow-md hover:bg-white/70 transition duration-200 text-lg w-50"
+            className="px-6 py-3 bg-white/70 backdrop-blur-sm text-gray-700 rounded-xl shadow-md hover:bg-white/30 transition duration-200 text-lg w-50"
           >
             Iniciar Sesión
           </button>
