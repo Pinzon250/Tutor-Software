94dfd0675055b07701c2cc94315cf7be2cd92c78
Pinzon250 <santiagopinzo@gmail.com>
2025-05-20 00:44:41 -0500

    Se actualizo el main.cjs para poder ejecutar el backend dentro del archivo .exe

--- frontend/electron/main.cjs
+++ frontend/electron/main.cjs
@@ -1,12 +1,15 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const { spawn } = require("child_process");
 const isDev = app.isPackaged ? false : require("electron-is-dev");
+
+let backendProcess;
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1780,
     height: 1050,
-    icon: path.join(__dirname, "../public/Tutor.ico"),
+    icon: path.join(__dirname, "../public/tutor.ico"),
     autoHideMenuBar: true,
     backgroundColor: "#2e2c29",
     titleBarStyle: "hiddenInset",
@@ -18,16 +21,46 @@ function createWindow() {
   if (isDev) {
     win.loadURL("http://localhost:5173");
   } else {
-
-    win.loadURL(`file://${path.join(__dirname, "../index.html")}`);
-    win.webContents.openDevTools();
+    win.loadURL(`file://${path.join(__dirname, "../dist/index.html")}`);
+    win.openDevTools();
   }
 }
 
-app.whenReady().then(createWindow);
+function startBackend() {
+  let backendPath;
+
+  if (isDev) {
+    backendPath = path.join(__dirname, "backend", "main.exe");
+  } else {
+    backendPath = path.join(process.resourcesPath, "app", "backend", "main.exe");
+  }
+
+  console.log("Backend path:", backendPath);
+
+  backendProcess = spawn(backendPath, [], {
+    detached: true,
+    stdio: "ignore",
+  });
+
+  backendProcess.unref();
+}
+
+function stopBackend() {
+  if (backendProcess) {
+    process.kill(-backendProcess.pid);
+  }
+}
+
+app.whenReady().then(() => {
+  startBackend();
+  createWindow();
+});
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+  if (process.platform !== "darwin") {
+    stopBackend();
+    app.quit();
+  }
 });
 
 app.on("activate", () => {
